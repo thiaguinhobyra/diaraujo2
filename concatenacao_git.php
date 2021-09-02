abbd6f564f4d48100868e6abc6199b1edc4d1d7f
tihaguinhobyra10 <thiago_m.araujo@hotmail.com>
2021-09-02 13:18:41 -0300

    Update concatenacao_git.php

--- concatenacao_git.php
+++ concatenacao_git.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <html>
 <head>
 	<meta charset="utf-8">
@@ -21,39 +20,10 @@
 		// aspas duplas
 		echo '<br />';
 
-		echo "Ola $nome";
-		echo "sua $idade";
-		echo "seu é $peso";
+		echo "Ola $nome, ";
+		echo "sua idade é $idade anos, ";
+		echo "seu peso é $peso kg, ";
+		echo "e você fuma $fumante carteira por dia.";
 	?>
 </body>
-=======
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Curso PHP</title>
-</head>
-<body>
-	<?php
-		//string
-		$nome = 'jorge sant ana';
-		//int
-		$idade = 35;
-		//float
-		$peso = 75.4;
-		//boolean
-		$fumante = true; //(true = 1) ou (false = vazio)
-		// logica
-		$idade = 36;
-		// operador .
-		echo 'ola ' . $nome . ', idade ' . $idade . ', peso ' . $peso . ' fumante ' . $fumante;
-		// aspas duplas
-		echo '<br />';
-
-		echo "Ola $nome";
-		echo "sua idade é $idade";
-		echo "seu peso é $peso";
-		echo "voce fuma $fumante";
-	?>
-</body>
->>>>>>> 204a0f25020fb6e8dcba411b58ad57686e0aeb9f
 </html>
