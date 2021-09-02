a15ce7724834c835e9c22a3cb2485b3a5d73bc7e
thiaguinhobyra <85735157+thiaguinhobyra@users.noreply.github.com>
2021-09-02 13:13:15 -0300

    Update concatenacao_git.php

--- concatenacao_git.php
+++ concatenacao_git.php
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Curso PHP</title>
-</head>
-<body>
-	<?php
-		//string
-		$nome = 'jorge sant ana';
-		//int
-		$idade = 35;
-		//float
-		$peso = 75.4;
-		//boolean
-		$fumante = true; //(true = 1) ou (false = vazio)
-		// logica
-		$idade = 36;
-		// operador .
-		echo 'ola ' . $nome . ', idade ' . $idade . ', peso ' . $peso . ' fumante ' . $fumante;
-		// aspas duplas
-		echo '<br />';
-
-		echo "Ola $nome";
-		echo "sua $idade";
-		echo "seu é $peso";
-	?>
-</body>
-=======
 <html>
 <head>
 	<meta charset="utf-8">
